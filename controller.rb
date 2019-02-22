4694c72ae33c0433dfd41efd8d9bbb182c3bd5c0
Kevin Frew <kevinfrew86@gmail.com>
2019-02-22 11:45:48 +0000

    removes sinatra contrib

--- controller.rb
+++ controller.rb
@@ -1,5 +1,4 @@
 require('sinatra')
-require('sinatra/contrib/all')
 require_relative('controllers/animals_controller.rb')
 require_relative('controllers/owner_controller.rb')
 require_relative('controllers/adoption_controller.rb')
